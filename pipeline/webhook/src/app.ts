52850dcaca0514678212d29c2acd47255f616089
Alberto Anceschi <anceschialberto@me.com>
2021-11-29 00:34:42 +0100

    webhook update

--- pipeline/webhook/src/app.ts
+++ pipeline/webhook/src/app.ts
@@ -3,7 +3,8 @@ import { APIGatewayEvent, Callback, Context } from "aws-lambda";
 // https://docs.github.com/en/developers/webhooks-and-events/webhooks/securing-your-webhooks
 export const handler = (event: APIGatewayEvent, ctx: Context, cb: Callback) => {
   console.info("Webhook handler invoked");
-  console.info(JSON.stringify(event, null, 2));
+  const body = JSON.parse(event.body ?? "") as unknown;
+  console.info(JSON.stringify(body, null, 2));
 
-  cb(null, { statusCode: 200, body: "" });
+  cb(null, { statusCode: 202, body: "" });
 };
