70ea7e0c68a84705a355d50708db8897a217a206
Alberto Anceschi <anceschialberto@me.com>
2021-11-30 10:06:37 +0100

    webhook skip ci

--- pipeline/webhook/src/app.ts
+++ pipeline/webhook/src/app.ts
@@ -1,3 +1,7 @@
+/* eslint-disable no-unsafe-optional-chaining */
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+
 import { APIGatewayEvent } from "aws-lambda";
 import {
   CodePipelineClient,
@@ -9,12 +13,19 @@ const codePipelineClient = new CodePipelineClient({});
 // https://docs.github.com/en/developers/webhooks-and-events/webhooks/securing-your-webhooks
 export const handler = async (event: APIGatewayEvent) => {
   console.info("Webhook handler invoked");
-  const body = JSON.parse(event.body ?? "") as unknown;
+  const body = JSON.parse(event.body ?? "{}") as Record<string, any>;
   console.info(JSON.stringify(body, null, 2));
+
+  if (body.sender?.login === "aws-connector-for-github[bot]") {
+    return {
+      statusCode: 202,
+      body: "AWS Connector bot detected, pipeline won't be triggered",
+    };
+  }
 
   await codePipelineClient.send(
     new StartPipelineExecutionCommand({ name: process.env.PIPELINE_NAME })
   );
 
-  return { statusCode: 202, body: "" };
+  return { statusCode: 202, body: "Pipeline triggered" };
 };
