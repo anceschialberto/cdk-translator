54a71c33394ff08ae9a3918e910ad5b8666d2562
Alberto Anceschi <anceschialberto@me.com>
2021-12-08 21:20:05 +0100

    test: update to v2

--- pipeline/test/pipeline.test.ts
+++ pipeline/test/pipeline.test.ts
@@ -1,16 +1,16 @@
 /* eslint-disable @typescript-eslint/no-unsafe-member-access */
 /* eslint-disable @typescript-eslint/no-unsafe-call */
 
-import { Template } from "@aws-cdk/assertions";
-import * as cdk from "@aws-cdk/core";
+import { assertions } from "aws-cdk-lib";
+import { App } from "aws-cdk-lib"; // core constructs
 import * as Pipeline from "../lib/pipeline-stack";
 
 test("Empty Stack", () => {
-  const app = new cdk.App();
+  const app = new App();
   const stack = new Pipeline.PipelineStack(app, "CdkDayStack-cicd");
 
   // Prepare the stack for assertions.
-  const template = Template.fromStack(stack);
+  const template = assertions.Template.fromStack(stack);
 
   // Assert the template matches the snapshot.
   expect(template.toJSON()).toMatchSnapshot();
